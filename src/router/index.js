946827239945ebbbab6f67a5d3e7a84a43c89c42
shyo21 <shyo21@kaist.ac.kr>
2022-01-21 01:56:52 +0900

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/learn",
+    name: "Learn",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Learn.vue"),
+  },
 ];
 
 const router = createRouter({
