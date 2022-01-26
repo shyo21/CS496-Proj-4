00a919114894492c83d76d06b38c38a2ddbfd5e3
shyo21 <shyo21@kaist.ac.kr>
2022-01-26 19:56:44 +0900

    wmo

--- src/components/AddImages.vue
+++ src/components/AddImages.vue
@@ -1,5 +1,8 @@
 <style scoped>
-.container img {
+.imagescontainer {
+  display: block;
+}
+.imagescontainer img {
   max-width: 200px;
   max-height: 200px;
   display: block;
@@ -7,16 +10,96 @@
   margin-top: 20px;
   margin-bottom: 20px;
 }
+.inputsCont {
+}
 .title {
+  text-align: center;
+
+  font-size: 100px;
   font-weight: 700;
+  font-family: "Apple SD Gothic Neo", Roboto, "Noto Sans KR", NanumGothic,
+    "Malgun Gothic", sans-serif;
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+}
+.submitCont {
+  position: absolute;
+  top: 85%;
+  left: 85%;
+}
+.nextButton {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  background: #ff5c5c;
+  color: #ffffff;
+
+  margin: 0 50px 0 0;
+  padding: 0.7rem 1rem;
+  height: auto;
+
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+
+  border: none;
+  border-radius: 20px;
+
+  display: inline-block;
+  flex-basis: 6rem;
+
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+  cursor: pointer;
+
+  transition: 0.5s;
+}
+.nextButton:active,
+.nextButton:hover {
+  background: #ff7a7a;
+  outline: 0;
+  flex-basis: 7rem;
+  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+    0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+.nextButton:disabled {
+  opacity: 0.5;
+}
+.nextButton span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+  color: #ffffff;
+}
+
+.nextButton span:after {
+  content: "\00bb";
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.nextButton:hover span {
+  padding-right: 25px;
+}
+
+.nextButton:hover span:after {
+  opacity: 1;
+  right: 0;
 }
 </style>
 
 <template>
-  <div class="container">
-    <p class="title">Single Image Preview</p>
+  <div class="imagescontainer">
+    <p class="title">3,2,1 Cheeze~</p>
     <hr />
-    <div class="large-12 medium-12 small-12 cell">
+    <div class="inputsCont">
       <label>
         File Preview
         <input
@@ -47,7 +130,11 @@
       <img v-bind:src="imagePreview_0" v-show="showPreview_0" />
       <img v-bind:src="imagePreview_1" v-show="showPreview_1" />
       <img v-bind:src="imagePreview_2" v-show="showPreview_2" />
-      <button v-on:click="submitFile()">Submit</button>
+      <div class="submitCont">
+        <button class="nextButton" @click="submitFile">
+          <span>Submit</span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
